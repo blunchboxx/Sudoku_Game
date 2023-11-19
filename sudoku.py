3dec30eaa02d4bb22fc2f3d6e4cac852fe1232c4
Jason S <jschinis@ufl.edu>
2023-11-19 10:45:03 -0500

    Initial updates to main()

--- sudoku.py
+++ sudoku.py
@@ -1,12 +1,18 @@
 from sudoku_generator import *
-import pygame, sys
+import pygame
+from constants import *
+import sys
 
 # main() (Tom/Jason).
 # create different screens (game start, game over, and game in-progress)
 
 
 if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode(WIDTH, HEIGHT)
+    pygame.display.set_caption('Sudoku')
     sudoku_board = SudokuGenerator(9, 30)
+    screen.fill(BG_COLOR)
 
     sudoku_board.fill_diagonal()
     sudoku_board.fill_remaining(0, 0)
