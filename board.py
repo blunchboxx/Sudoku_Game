43d0c54060f2a49a05f7519cb60c903ae4050057
Corey <coreycava22@gmail.com>
2023-11-20 21:57:20 -0500

    minor edits to board.py

--- board.py
+++ board.py
@@ -16,26 +16,37 @@ class Board:
     def click(self, x, y):
         pass
 
-    def clear(self):
+    def clear(self): #IN WORK: Corey 11/20/23
+        #check if cell was empty at game start
+            #set_cell_value(0) (cell class) #update to tell specify which cell
         pass
 
-    def sketch(self, value):
+    def sketch(self, value): #IN WORK: Corey 11/20/23
+
+
+        #update once draw() is finished
         pass
 
-    def place_number(self, value):
+    def place_number(self, value): #IN WORK: Corey 11/20/23
+        # set_cell_value(value) (cell class) #UPDATE to tell specify which cell
+
         pass
 
-    def reset_to_original(self):
+    def reset_to_original(self): #IN WORK: Corey 11/20/23
+        #for loop that goes through original 2-d array and assigns corresponding values
         pass
 
-    def is_full(self):
+    def is_full(self): #IN WORK: Corey 11/20/23
+        #for loop that steps through 2-d array and checks for 0's
         pass
 
-    def update_board(self):
+    def update_board(self): #IN WORK: Corey 11/20/23
         pass
 
-    def find_empty(self):
+    def find_empty(self): #IN WORK: Corey 11/20/23
+        #for loop through current aray and looks for 0
         pass
 
-    def check_board(self):
+    def check_board(self): #IN WORK: Corey 11/20/23
+        #for loop that compares each element to corresponding element in solution 2-d array
         pass
